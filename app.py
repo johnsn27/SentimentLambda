58ab9cd584d79b47fb81149c98b74dad575eaad9
johnsn27 <nathan.johnson@bbc.co.uk>
2021-02-26 17:09:07 +0000

    Create Sentiment Response

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
+from threading import Thread
+
+from flask import Flask, jsonify, make_response, request
+from response import response
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
- return '<h1>Hello World</h1>'
+def sentiment_analysis():
+    """"method that outputs a response"""
+    if request.method == 'GET':
+        text = 'happy to the point of sadness'
+        return response(text)
+    return make_response(jsonify({'error': 'sorry! unable to parse', 'status_code': 500}), 500)
 
 # We only need this for local development.
 if __name__ == '__main__':
